b587083fb72729ea05578ef452ba9e744f65d225
azumahjoshua <joshuaazumah1086@gmail.com>
2023-01-11 20:39:18 +0100

    error validation added

--- Hotelms.py
+++ Hotelms.py
@@ -1,88 +1,89 @@
 from datetime import date
 
-from dishes import dishes
-# 
-rooms = {
-    "one":2000,
-    "two":3000,
-    "three":4000,
-    "four":5000
-}
-
+from index import dishes,rooms,games,laundary
 
 class Hotelms:
-
-    def __init__(self, user_name ='',user_address='',checkindate = '',checkoutdate = '',dayspent = 0, total_bill=0,totalroomrent=0,totalrestaurantbill=[]):
-         self.user_name =user_name
-         self.user_address = user_address
-         self.checkindate = checkindate 
-         self.checkoutdate = checkoutdate
-         self.dayspent = dayspent
-         self.total_bill = total_bill
-         self.totalroomrent = totalroomrent
-         self.totalrestaurantbill = totalrestaurantbill
+    """
+    A class for managing a hotel guest's stay, including their name, 
+    check-in and check-out dates, and the total cost of their stay.
+    """
+    # initillise data
+    def __init__(self):
+         self.user_name =''
+         self.user_address = ''
+         self.checkindate = None
+         self.checkoutdate = None
+         self.dayspent = 0
+         self.total_bill = 0
+         self.totalroomrent = 0
+         self.totalrestaurantbill = 0
+         self.totallaundarybill = 0
+         self.totalgamebill = 0
 
     # A function to get user data name, address,checkout dates and checking in dates. The function also calculate the days to be spent by the user
-    def customerData(self):
+    def get_customer_data(self):
+        """
+        Prompts the user for their name, address, and check-in and check-out dates, 
+        and calculates the number of days the guest will be staying at the hotel.
+        """
+
         self.user_name = input('Please enter your name: ')
         self.user_address = input('Please enter your address: ')
-        checkinyear = int(input('Enter checkin year: '))
-        checkinmonth = int(input('Enter checkin month: '))
-        checkinday = int(input('Enter checkin day: '))
-        self.checkindate= date(checkinyear, checkinmonth, checkinday)
-        checkoutyear = int(input('Enter checkout year: '))
-        checkoutmonth = int(input('Enter checkout month: '))
-        checkoutday = int(input('Enter checkout day: '))
-        self.checkoutdate= date(checkoutyear, checkoutmonth, checkoutday)
-
-        while (self.checkindate >= self.checkoutdate):
-            print("Error inputing Date: Checking out should be greater than checking in")
+        try :
             checkinyear = int(input('Enter checkin year: '))
             checkinmonth = int(input('Enter checkin month: '))
             checkinday = int(input('Enter checkin day: '))
             self.checkindate= date(checkinyear, checkinmonth, checkinday)
             checkoutyear = int(input('Enter checkout year: '))
             checkoutmonth = int(input('Enter checkout month: '))
-            checkoutday = int(input('Enter checkin day: '))
+            checkoutday = int(input('Enter checkout day: '))
             self.checkoutdate= date(checkoutyear, checkoutmonth, checkoutday)
-        self.dayspent = self.checkoutdate - self.checkindate
-        self.dayspent = self.dayspent.days
+        except ValueError as error:
+            print("Invalid date input:", error)
+            return
 
 
-
+        if self.checkindate >= self.checkoutdate:
+            raise ValueError("Error: Check-out date must be later than check-in date.")
+        
+        self.days_spent = (self.check_out_date - self.check_in_date).days
+        print(f"Total days spent: {self.days_spent}")
+        
+        
 
     
     # Calculate the rent to be paid 
-    def calculateRoomRent(self):
-        roomnumber = int(input("Select room number between 1 and 4: "))
-        roomprice = 0
-        totaldayspent = self.dayspent
-        # print(totaldayspent.days)
+    def calculate_room_rent(self):
+        """
+        Prompts the user to select a room number, and calculates the cost of the room rental
+        based on the number of days the guest will be staying.
+        """
         try:
-            if(roomnumber<=0 or roomnumber >=5):
-                print("please choose between the numbers 1 to 4")
-                exit()  #to quit the program
-            elif(roomnumber == 1):
-                roomprice = rooms['one']
-            elif(roomnumber == 2):
-                roomprice = rooms['two']
-            elif(roomnumber == 3):
-                roomprice = rooms['three']
-            elif(roomnumber == 4):
-                roomprice = rooms['four']
-            
+            roomnumber = int(input("Select room number between 1 and 4: "))
+            room_price = rooms['roomnumber']
+            if not room_price:
+                raise ValueError("Invalid room number. Please choose between 1 and 4.")
+            totaldayspent = self.dayspent
+
             print(f"Room Number: {roomnumber}")
-            print(f"Room Price: {roomprice*totaldayspent}")
+            print(f"Room Price: {room_price*totaldayspent}")
             print("------------------------------------------\n")
 
-            self.totalroomrent = roomprice*totaldayspent
+            self.totalroomrent = room_price*totaldayspent
         except ValueError:
             print("Room Number must be an integer")
+            return
 
         
-    def restaurantBill(self):
+        # Calculate the restaurant bill
+    def calculate_restaurant_bill(self):
+        """
+        Prompts the user to select a food id from the hotel restaurant menu, 
+        the user can aslo select multiple foods and the price is  stored in a list.
+        the function calculates the total bills in te restaurant
+        """
         # print menu
-        print(len(dishes))
+        # print(len(dishes))
         for index in range(len(dishes)):
             for key,val in dishes[index].items():
                 print("{} : {}".format(key, val))
@@ -101,32 +102,74 @@ class Hotelms:
             # print(sum(self.totalrestaurantbill))
         except IndexError:
             print("Index not found")
-        
+    
+    # Calculate laundary bill
+    def calculate_laundary_bill(self):
+        """
+        Prompts the user to select a lundary list, the user can aslo select multiple lundary list and the price is  stored in a list.
+        the function calculates the total bills
+        """
 
+        for index in range(len(laundary)):
+            for key,val in laundary[index].items():
+                print("{} : {}".format(key, val))
+            print("-------------------------------------------\n")
+            # choose laundary id
+        laundayId = int(input("Select the laundary id form the laundary list above: "))
+        try:
+            choosenitem = [i for i in laundary if i['id'] == laundayId][0]
+            print("{} : {}".format("Launday Item", "Price $"))
+            print("------------------------------------------")
+            print("{} : {}".format(choosenitem['name'], choosenitem['price']))
+            print("-------------------------------------------\n")
+            self.totallaundarybill.append(choosenitem['price'])
+        except IndexError:
+            print("Index not found")
 
-    # def laundaryBill():
-        # pass
-    # def gameBill():
-        # pass
-    def totalExpenditure(self):
-        # room_rent = self.calculateRoomRent()
-        # restaurant_bill = self.restaurantBill()
-        # Add calls to other bill calculation methods here
-        # bills = 0
-        self.total_bill = sum(self.totalrestaurantbill) + self.totalroomrent
+# calculate game bill
+    def calculate_game_bill(self):
+        """
+        Prompts the user to select a game of choice, and calculate the total game bill
+
+        """
+        for index in range(len(games)):
+            for key,val in games[index].items():
+                print("{} : {}".format(key, val))
+        print("-------------------------------------------\n")
+        gameId = int(input("Select the game id form the game list above: "))
+        try:
+            choosenitem = [i for i in games if i['id'] == gameId][0]
+            print("{} : {}".format("Game Item", "Price $"))
+            print("------------------------------------------")
+            print("{} : {}".format(choosenitem['name'], choosenitem['price']))
+            print("-------------------------------------------\n")
+            self.totalgamebill.append(choosenitem['price'])
+        except IndexError:
+            print("Index not found")
+
+    # totale expenditure
+    def generate_bill(self):
+        """
+        Calculate the total expenditure of the user in the hotel
+        """
+        self.total_bill = sum(self.totalrestaurantbill) + self.totalroomrent + sum(self.totalgamebill)
+        + sum(self.totallaundarybill)
         # Add up the results of all the bill calculation methods
         # bills += self.total_bill
-        return self.total_bill 
+        print(f"Total Bill: {self.total_bill}")
+        
 
 
 print("-------------------------- Hotel-Management-System Menu Options -----------------------------------------")
 
 menu_options = {
     1: '(User Data: )Option 1',
-    2: '(Select Room: )Option 2',
+    2: '(Select Room Number: )Option 2',
     3: '(Select Restaurants: )Option 3',
-    4: '(Total Expenditure: )Option 4',
-    5: 'Exit',
+    4: '(Select Game: )Option 4',
+    5: '(Select Laundary: )Option 5',
+    6: '(Total Expenditure: )Option 6',
+    7: 'Exit',
 }
 
 # create an object of  a user
@@ -143,6 +186,7 @@ if __name__=='__main__':
         option = ''
         try:
             option = int(input('Enter your choice: '))
+            print("------------------------------------------\n")
         except:
             print('Wrong input. Please enter a number ...')
         #Check what choice was entered and act accordingly
@@ -153,12 +197,16 @@ if __name__=='__main__':
         elif option == 3:
             user.restaurantBill()
         elif option == 4:
-             print("------------------------------------------\n")
-             print(f"Total Bill: {user.totalExpenditure()}")
-             print("------------------------------------------\n")
+            user.gameBill()
         elif option == 5:
+            user.laundaryBill()
+        elif option == 6:
+             print("------------------------------------------\n")
+             user.generate_bill()
+             print("------------------------------------------\n")
+        elif option == 7:
             print("------------------------------------------\n")
-            print('Thanks message before exiting')
+            print('Thanks message visiting our hotel')
             print("------------------------------------------\n")
             exit()
         else:
--- index.py
+++ index.py
@@ -1,22 +1,115 @@
-ages = [1,2,3,4,5] # list
-# print(ages)
-
-person = {
-    "name":"lina",
-    "age":21
-}
-
-[
-{
-  "name":"lina",
- "age":21
-},
-{
- "name":"lina",
- "age":21
-}
-
+dishes = [
+  {
+    "description":
+      "These mini cheeseburgers are served on a fresh baked pretzel bun with lettuce, tomato, avocado, and your choice of cheese. ",
+    "title": "Mini Cheeseburgers",
+    "course": "Starters",
+    "id": 1,
+    "price": 8,
+  },
+  {
+    "description":
+      "Large mushroom caps are filled a savory cream cheese, chicken bacon and panko breadcrumb stuffing, topped with cheddar cheese. ",
+    "title": "Panko Stuffed Mushrooms",
+    "course": "Starters",
+    "id": 2,
+    "price": 7,
+  },
+  {
+    "description":
+      "Our New York Style Cheesecake is rich, smooth, and creamy. Available in various flavors, and with seasonal ",
+    "title": "Cheesecake",
+    "course": "Desserts",
+    "id": 3,
+    "price": 9,
+  },
+  {
+    "description":
+      "This platter is perfect for sharing! Enjoy our spicy buffalo wings, traditional nachos, and cheese quesadillas served with freshly made guacamole dip.",
+    "title": "Fiesta Family Platter",
+    "course": "Entrees",
+    "id": 4,
+    "price": 16,
+  },
+  {
+    "description":
+      "This classic cheese tortellini is cooked in a sundried tomato sauce. Served with bruschetta topped with a tomato and basil marinara.",
+    "title": "Tomato Bruschetta Tortellini",
+    "course": "Entrees",
+    "id": 5,
+    "price": 14,
+  },
+  {
+    "description":
+      "Elegantly crafted creamy vanilla custard with a caramelized crunchy layer on top. Served with seasonal fruit.",
+    "title": "CrÃ¨me BrÃ»lÃ©e",
+    "course": "Desserts",
+    "id": 6,
+    "price": 9,
+  },
+  {
+    "description":
+      "Made with local granny smith apples to bring you the freshest classic apple pie available.",
+    "title": "Apple Pie",
+    "course": "Desserts",
+    "id": 7,
+    "price": 5,
+  },
+  {
+    "description":
+      "Our thin crust pizzas are made fresh daily and topped with your choices of fresh meats, veggies, cheese, and sauce. Price includes two toppings. Add $1 for each addition",
+    "title": "Handcrafted Pizza",
+    "course": "Entrees",
+    "id": 8,
+    "price": 10,
+  },
+  {
+    "description":
+      "Our barbecued skewers include tofu, cherry tomatoes, bell peppers, and zucchini marinated in a ginger sesame sauce and charbroiled. Served with steamed rice.",
+    "title": "Barbecued Tofu Skewers",
+    "course": "Entrees",
+    "id": 9,
+    "price": 10,
+  }
 ]
 
-for i in range(len(ages)):
-    print(i)
+rooms = {
+    "one":2000,
+    "two":3000,
+    "three":4000,
+    "four":5000
+}
+
+games = [
+    {
+    "id":1,
+    "name": "Table tennis",
+    "price": 60
+    },
+    {
+    "id":2,
+    "name":"Bowling",
+    "price": 80
+    },
+    {
+    "id":3,
+    "name":"Snooker",
+    "price":70
+    },
+    {"id":4,
+    "name":"Video games",
+    "price":90
+    },
+    {"id":5,
+    "name":"Pool",
+    "price":50
+    }
+]
+
+laundary = [
+    {"id":1, "name": "Shorts", "price": 3},
+    {"id":2, "name": "Trousers", "price": 4},
+    {"id":3, "name": "Shirts", "price": 5},
+    {"id":4, "name": "Jeans", "price": 6},
+    {"id":5, "name": "Girlsuit", "price": 7},
+]
